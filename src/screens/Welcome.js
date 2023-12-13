61dc188023d45e9f9d9f02ba0b7136f9c2091a95
gidbecxa <gidbec4yeshua@gmail.com>
2023-12-13 23:24:26 +0100

    Fixed error with importing produce from immer

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: GlobalColors.background,
+        backgroundColor: "#FFFFFF",
         paddingHorizontal: 12,
     },
 })
